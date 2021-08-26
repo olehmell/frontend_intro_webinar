7da85facec7d1373a5b9f3020ea969012ac89226
Oleh Mell <oleh.melnechyk@gmail.com>
2021-08-26 20:14:27 +0300

    Small broken

--- js/nodes.js
+++ js/nodes.js
@@ -1,61 +1,59 @@
 const moviesContainer = document.getElementById('movies')
 
 const cleanContainer = () => {
-  moviesContainer.innerHTML = ''
+    moviesContainer.innerHTML = ''
 }
 
 const buildMovie = ({ Poster, Title, Year }) => {
-  const e = document.createElement('div')
-  e.setAttribute('class', 'movie-item')
+    const e = document.createElement('div')
+    e.setAttribute('class', 'movie-item')
 
-  e.innerHTML = `
+    e.innerHTML = `
     <h4 class="title">${Title} (${Year})</h4>
     <img src=${Poster} width="300" height="445" alt="poster" />
   `
-  
-  return e
+
+    return e
 }
 
 const buildImgContainer = (path, className, alt) => {
-  const div = document.createElement('div')
-  div.setAttribute('class', className)
-  const img = document.createElement('img')
+    const div = document.createElement('div')
+    div.setAttribute('class', className)
+    const img = document.createElement('img')
 
-  cleanContainer()
-  img.setAttribute('class', className)
-  img.setAttribute('alt', alt)
-  img.setAttribute('src', path)
-  img.setAttribute('width', '100')
-  img.setAttribute('height', '100')
+    cleanContainer()
+    img.setAttribute('class', className)
+    img.setAttribute('alt', alt)
+    img.setAttribute('src', path)
+    img.setAttribute('width', '100')
+    img.setAttribute('height', '100')
 
-  div.appendChild(img)
+    div.appendChild(img)
 
-  return div
+    return div
 }
 
 export const startLoading = () => {
-  const loading = buildImgContainer('img/loading.svg', 'loading', 'loading...')
-  moviesContainer.appendChild(loading)
+    const loading = buildImgContainer('img/loading.svg', 'loading', 'loading...')
+    moviesContainer.appendChild(loading)
 }
 
 export const buildError = (errorMsg) => {
-  cleanContainer()
+    cleanContainer()
 
-  const error = buildImgContainer('img/error.svg', 'error', 'error')
+    const error = buildImgContainer('img/error.svg', 'error', 'error')
 
-  const span = document.createElement('span')
-  span.setAttribute('class', 'error')
-  span.innerText = errorMsg
+    const span = document.createElement('span')
+    span.setAttribute('class', 'error')
+    span.innerText = errorMsg
 
-  error.appendChild(span)
+    error.appendChild(span)
 
-  moviesContainer.appendChild(error)
+    moviesContainer.appendChild(error)
 }
 
 export const buildMovies = (moviesData) => {
-  cleanContainer()
+    cleanContainer()
 
-  moviesData
-    .map(buildMovie)
-    .forEach(node => moviesContainer.appendChild(node))
+    moviesData.map(data => buildMovie(data)).forEach(node => moviesContainer.appendChild(node))
 }
--- js/search.js
+++ js/search.js
@@ -2,28 +2,29 @@ import { buildError, buildMovies, startLoading } from './nodes.js'
 import { createOmdbApiUrl, MOVIES_KEY } from './utils.js'
 
 const renderSavedMovies = () => {
-  const maybeMovies = JSON.parse(localStorage.getItem(MOVIES_KEY))
+    const maybeMovies = JSON.parse(localStorage.getItem(MOVIES_KEY))
 
-  if (maybeMovies) return buildMovies(maybeMovies)
+    if (maybeMovies) return buildMovies(maybeMovies)
 }
 
 const searchByTitle = () => {
-  const form = document.forms.search;
+    const form = document.forms.search;
 
-  const value = form.elements.searchValue.value;
+    const value = form.elements.searchValue.value;
 
-  startLoading()
+    startLoading()
 
-  fetch(createOmdbApiUrl([{ param: 's', value }]))
-    .then(response => response.json())
-    .then(jsonResponse => {
-      if (jsonResponse.Response === "True") {
-        localStorage.setItem(MOVIES_KEY, JSON.stringify(jsonResponse.Search))
-        buildMovies(jsonResponse.Search)
-      } else {
-        buildError(jsonResponse.Error);
-      }
-    })
+    fetch(createOmdbApiUrl([{ param: 's', value }]))
+        .then(response => response.json())
+        .then(jsonResponse => {
+            console.log('Response', jsonResponse)
+            if (jsonResponse.Response === "True") {
+                localStorage.setItem(MOVIES_KEY, JSON.stringify(jsonResponse.Search))
+                buildMovies(jsonResponse.Search)
+            } else {
+                buildError(jsonResponse.Error);
+            }
+        })
 }
 
 renderSavedMovies()
